38ea443f284648778232a62235faa3026aa96f3d
ShoooGit <shooo.ity.777@gmail.com>
2020-12-13 19:48:18 +0900

    ハンドラの作成

---
+++ src/gin/handler/articleFunc.go
@@ -0,0 +1,35 @@
+package handler
+
+import (
+	"gin/article"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func ArticlesGet(articles *article.Articles) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		result := articles.GetAll()
+		ctx.JSON(http.StatusOK, result)
+	}
+}
+
+type ArticlePostRequest struct {
+	Title       string `json:"title"`
+	Description string `json:"description`
+}
+
+func ArticlePost(post *article.Articles) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		requestBody := ArticlePostRequest{}
+		ctx.Bind(&requestBody)
+
+		item := article.Item{
+			Title:       requestBody.Title,
+			Description: requestBody.Description,
+		}
+		post.Add(item)
+
+		ctx.Status(http.StatusNoContent)
+	}
+}
