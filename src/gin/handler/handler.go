2000e92fa4c55d78ec0bbdcd8c3d0b89ca649e90
ShoooGit <shooo.ity.777@gmail.com>
2020-12-13 23:34:23 +0900

    mainの処理をハンドラに移動

--- src/gin/handler/handler.go
+++ src/gin/handler/handler.go
@@ -2,11 +2,83 @@ package handler
 
 import (
 	"gin/article"
+	"gin/dblib"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
+func Index() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		todos := dblib.GetAll()
+		ctx.HTML(200, "index.html", gin.H{
+			"todos": todos,
+		})
+	}
+}
+
+func Create() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		text := ctx.PostForm("text")
+		status := ctx.PostForm("status")
+		dblib.Insert(text, status)
+		ctx.Redirect(302, "/")
+	}
+}
+
+func Detail() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		n := ctx.Param("id")
+		id, err := strconv.Atoi(n)
+		if err != nil {
+			panic(err)
+		}
+		todo := dblib.GetOne(id)
+		// ctx.HTML(200, "detail.html", gin.H{"todo": todo})
+		ctx.JSON(http.StatusOK, todo)
+	}
+}
+
+func Update() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		n := ctx.Param("id")
+		id, err := strconv.Atoi(n)
+		if err != nil {
+			panic("ERROR")
+		}
+		text := ctx.PostForm("text")
+		status := ctx.PostForm("status")
+		dblib.Update(id, text, status)
+		ctx.Redirect(302, "/")
+	}
+}
+
+func CheckDelete() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		n := ctx.Param("id")
+		id, err := strconv.Atoi(n)
+		if err != nil {
+			panic("ERROR")
+		}
+		todo := dblib.GetOne(id)
+		ctx.HTML(200, "delete.html", gin.H{"todo": todo})
+	}
+}
+
+func Delete() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		n := ctx.Param("id")
+		id, err := strconv.Atoi(n)
+		if err != nil {
+			panic("ERROR")
+		}
+		dblib.Delete(id)
+		ctx.Redirect(302, "/")
+	}
+}
+
+/////////////////////////////////////////////////////////////////////////////////
 func ArticlesGet(articles *article.Articles) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		result := articles.GetAll()
@@ -33,3 +105,5 @@ func ArticlePost(post *article.Articles) gin.HandlerFunc {
 		ctx.Status(http.StatusNoContent)
 	}
 }
+
+/////////////////////////////////////////////////////////////////////////////////
--- src/gin/main.go
+++ src/gin/main.go
@@ -4,8 +4,6 @@ import (
 	"gin/article"
 	"gin/dblib"
 	"gin/handler"
-	"net/http"
-	"strconv"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/mattn/go-sqlite3"
@@ -18,6 +16,24 @@ func main() {
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*.html")
 
+	//Index
+	router.GET("/", handler.Index())
+
+	//Create
+	router.POST("/new", handler.Create())
+
+	//Detail
+	router.POST("/detail/:id", handler.Detail())
+
+	//Update
+	router.POST("/update/:id", handler.Update())
+
+	//削除確認
+	router.GET("/delete_check/:id", handler.CheckDelete())
+
+	//Delete
+	router.POST("/delete/:id", handler.Delete())
+
 	///////////////////////////////////////////////////
 	article := article.New()
 	// json
@@ -25,68 +41,6 @@ func main() {
 	router.POST("/article", handler.ArticlesGet(article))
 	///////////////////////////////////////////////////
 
-	//Index
-	router.GET("/", func(ctx *gin.Context) {
-		todos := dblib.GetAll()
-		ctx.HTML(200, "index.html", gin.H{
-			"todos": todos,
-		})
-	})
-
-	//Create
-	router.POST("/new", func(ctx *gin.Context) {
-		text := ctx.PostForm("text")
-		status := ctx.PostForm("status")
-		dblib.Insert(text, status)
-		ctx.Redirect(302, "/")
-	})
-
-	//Detail
-	router.GET("/detail/:id", func(ctx *gin.Context) {
-		n := ctx.Param("id")
-		id, err := strconv.Atoi(n)
-		if err != nil {
-			panic(err)
-		}
-		todo := dblib.GetOne(id)
-		// ctx.HTML(200, "detail.html", gin.H{"todo": todo})
-		ctx.JSON(http.StatusOK, todo)
-	})
-
-	//Update
-	router.POST("/update/:id", func(ctx *gin.Context) {
-		n := ctx.Param("id")
-		id, err := strconv.Atoi(n)
-		if err != nil {
-			panic("ERROR")
-		}
-		text := ctx.PostForm("text")
-		status := ctx.PostForm("status")
-		dblib.Update(id, text, status)
-		ctx.Redirect(302, "/")
-	})
-
-	//削除確認
-	router.GET("/delete_check/:id", func(ctx *gin.Context) {
-		n := ctx.Param("id")
-		id, err := strconv.Atoi(n)
-		if err != nil {
-			panic("ERROR")
-		}
-		todo := dblib.GetOne(id)
-		ctx.HTML(200, "delete.html", gin.H{"todo": todo})
-	})
-
-	//Delete
-	router.POST("/delete/:id", func(ctx *gin.Context) {
-		n := ctx.Param("id")
-		id, err := strconv.Atoi(n)
-		if err != nil {
-			panic("ERROR")
-		}
-		dblib.Delete(id)
-		ctx.Redirect(302, "/")
-
-	})
 	router.Run()
+
 }
