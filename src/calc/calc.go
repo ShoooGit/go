76ea12fc0e919ebf5c5c3753f989a918b06927c2
ShoooGit <shooo.ity.777@gmail.com>
2021-01-01 16:25:27 +0900

    生成された関数を利用したエラー結果の作成

--- src/calc/calc.go
+++ src/calc/calc.go
@@ -3,6 +3,7 @@ package calcapi
 import (
 	calc "calc/gen/calc"
 	"context"
+	"fmt"
 	"log"
 )
 
@@ -24,5 +25,9 @@ func (s *calcsrvc) Add(ctx context.Context, p *calc.AddPayload) (res int, err er
 
 // Divide implements divide.
 func (s *calcsrvc) Divide(ctx context.Context, p *calc.DividePayload) (res int, err error) {
+	if p.B == 0 {
+		// 生成された関数を利用してエラー結果を作成する
+		return 0, calc.MakeDivByZero(fmt.Errorf("right operand cannot be 0"))
+	}
 	return p.A / p.B, nil
 }
