6ef997514845d958059b714590e67cccf2d86eac
ShoooGit <shooo.ity.777@gmail.com>
2021-01-03 22:44:02 +0900

    APIデザイン

---
+++ src/sample1/design/design.go
@@ -0,0 +1,26 @@
+package design
+
+import (
+	. "goa.design/goa/v3/dsl"
+	cors "goa.design/plugins/v3/cors/dsl"
+)
+
+// API 定義
+var _ = API("goa3-sample", func() {
+	Title("SampleAPI")
+	Description("goa3 sample code.")
+
+	cors.Origin("/.*localhost.*/", func() {
+		cors.Headers("content-type")
+		cors.Methods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+		cors.MaxAge(600)
+	})
+
+	Server("goa3-sample", func() {
+		Services("Users", "Viron", "Admin")
+		Host("localhost", func() {
+			Description("development host")
+			URI("http://localhost:8080")
+		})
+	})
+})
--- src/sample1/go.mod
+++ src/sample1/go.mod
@@ -3,7 +3,8 @@ module sample1
 go 1.15
 
 require (
-	goa.design/goa/v3 v3.2.6 // indirect
+	goa.design/goa/v3 v3.2.6
+	goa.design/plugins/v3 v3.2.6
 	golang.org/x/mod v0.4.0 // indirect
 	golang.org/x/net v0.0.0-20201224014010-6772e930b67b // indirect
 	golang.org/x/sys v0.0.0-20201231184435-2d18734c6014 // indirect
