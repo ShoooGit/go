070e8227f065bdbd7aa59c4a848d93eaa38db9ec
ShoooGit <shooo.ity.777@gmail.com>
2021-01-04 13:47:47 +0900

    vironの設定

--- src/sample1/viron.go
+++ src/sample1/viron.go
@@ -27,5 +27,81 @@ func (s *vironsrvc) Authtype(ctx context.Context) (res viron.VironAuthtypeCollec
 func (s *vironsrvc) VironMenuEndpoint(ctx context.Context) (res *viron.VironMenu, err error) {
 	res = &viron.VironMenu{}
 	s.logger.Print("viron.viron_menu")
+	cl := "green"
+	th := "standard"
+	pk := "id"
+	pagenation := true
+
+	res = &viron.VironMenu{
+		Name: "goa3-sample Admin Screen",
+		Tags: []string{
+			"local",
+		},
+		Color: &cl,
+		Theme: &th,
+		Pages: []*viron.VironPage{
+			&viron.VironPage{
+				Section: "dashboard",
+				Name:    "ダッシュボード",
+				ID:      "quickview",
+				Components: []*viron.VironComponent{
+					&viron.VironComponent{
+						Name: "Users(bar)",
+						API: &viron.VironAPI{
+							Method: "get",
+							Path:   "/api/v1/admin/user_number",
+						},
+						Style: "graph-bar",
+					},
+				},
+			},
+			&viron.VironPage{
+				Section: "manage",
+				ID:      "user-admin",
+				Name:    "ユーザ管理",
+				Components: []*viron.VironComponent{
+					&viron.VironComponent{
+						API: &viron.VironAPI{
+							Method: "get",
+							Path:   "/api/v1/admin/users",
+						},
+						Name:       "ユーザ一覧",
+						Style:      "table",
+						Primary:    &pk,
+						Pagination: &pagenation,
+						Query: []*viron.VironQuery{
+							&viron.VironQuery{
+								Key:  "id",
+								Type: "integer",
+							},
+							&viron.VironQuery{
+								Key:  "name",
+								Type: "string",
+							},
+							&viron.VironQuery{
+								Key:  "email",
+								Type: "string",
+							},
+							&viron.VironQuery{
+								Key:  "created_at",
+								Type: "string",
+							},
+							&viron.VironQuery{
+								Key:  "updated_at",
+								Type: "string",
+							},
+						},
+						TableLabels: []string{
+							"id",
+							"name",
+							"email",
+							"created_at",
+							"updated_at",
+						},
+					},
+				},
+			},
+		},
+	}
 	return
 }
